97fddecbd83adf0dda9834c293141cd2063dad56
Serhat Cillidag <serhat@intenseye.com>
2018-11-15 21:36:08 +0300

    Remove frame selection 😉

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-if [${TYPE} = 'VIDEO']
+echo "Type is ${TYPE}"
+echo "URL is ${INPUT_VIDEO_FILE_URL}"
+if ["${TYPE}" = 'CAMERA']
 then
-  echo "Downloading ${INPUT_VIDEO_FILE_URL} from S3"
-  aws s3 cp ${INPUT_VIDEO_FILE_URL} . --region ${AWS_REGION}
-  ffmpeg -i ${INPUT_VIDEO_FILE_URL} -ss ${POSITION_TIME_DURATION} -vframes 1 -an -y ${OUTPUT_THUMBS_FILE_NAME}
-else
-  # Type is camera
   ffmpeg -i ${INPUT_VIDEO_FILE_URL} -vframes 1 -an -y ${OUTPUT_THUMBS_FILE_NAME}
+else
+  # Type is video
+  echo "Downloading ${INPUT_VIDEO_FILE_URL} from S3"
+  aws s3 cp ${INPUT_VIDEO_FILE_URL} /tmp/video --region ${AWS_REGION}
+  ffmpeg -i /tmp/video -vframes 1 -an -y ${OUTPUT_THUMBS_FILE_NAME}
 fi
 echo "Copying ${OUTPUT_THUMBS_FILE_NAME} to S3 at ${OUTPUT_S3_PATH}/${OUTPUT_THUMBS_FILE_NAME} ..."
 aws s3 cp ./${OUTPUT_THUMBS_FILE_NAME} s3://${OUTPUT_S3_PATH}/${OUTPUT_THUMBS_FILE_NAME} --region ${AWS_REGION}

0275eba74f6c4ea91da3d5d53db112c816db6693
Serhat Cillidag <serhat@intenseye.com>
2018-11-16 10:49:15 +0300

    Add public statement

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ then
 else
   # Type is video
   echo "Downloading ${INPUT_VIDEO_FILE_URL} from S3"
-  aws s3 cp ${INPUT_VIDEO_FILE_URL} /tmp/video --region ${AWS_REGION}
+  aws s3 cp ${INPUT_VIDEO_FILE_URL} /tmp/video --region ${AWS_REGION} --acl public-read
   ffmpeg -i /tmp/video -vframes 1 -an -y ${OUTPUT_THUMBS_FILE_NAME}
 fi
 echo "Copying ${OUTPUT_THUMBS_FILE_NAME} to S3 at ${OUTPUT_S3_PATH}/${OUTPUT_THUMBS_FILE_NAME} ..."
